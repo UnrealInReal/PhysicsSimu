d57b3decbcf4be73cb025950f02e6f77de61ff55
liuxiaonan <xiaonan.liu@motphys.com>
2023-06-20 11:44:33 +0800

    better integrate model for gravity

--- src/integrate.rs
+++ src/integrate.rs
@@ -1,6 +1,7 @@
 use super::components::*;
 
 pub fn integrate(state: &mut PhysicsState, dt: f32, a: glam::Vec3) {
-    state.translation.t += state.linear_velocity.v * dt;
+    let previous_v = state.linear_velocity.v;
     state.linear_velocity.v += a * dt;
+    state.translation.t += (state.linear_velocity.v + previous_v) * dt / 2.;
 }
